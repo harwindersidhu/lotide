dda3a887df02be55961be9e35e8e083b4b3bb2bc
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-03-28 00:40:09 -0600

    First commit

---
+++ middle.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual  = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const middle = function(array) {
+  let middleArray = [];
+  if (array.length <= 2) {
+    return middleArray;
+  } else if (array.length % 2 === 0) {
+    let middleIndex = Math.floor(array.length / 2);
+    middleArray.push(array[middleIndex - 1]);
+    middleArray.push(array[middleIndex]);
+  } else if (array.length % 2 !== 0) {
+    let middleIndex = Math.floor(array.length / 2);
+    middleArray.push(array[middleIndex]);
+  }
+  return middleArray;
+};
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
