c7286ee89c4a6d8c495fd7b1cf252cfeaa07fce2
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-03-28 00:22:53 -0600

    First commit

---
+++ flatten.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual  = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+  }
+  
+};
+
+const flatten = function(array) {
+  let flattenArray = [];
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      for (let elm of element) {
+        flattenArray.push(elm);
+      }
+    } else {
+      flattenArray.push(element);
+    }
+  }
+  return flattenArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
