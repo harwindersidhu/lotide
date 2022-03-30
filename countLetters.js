62a1cf06e99f4721033c80f5a13ca91b67185c98
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-03-29 23:18:02 -0600

    lotide count letters in a sentence

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const countLetters = function(sentence) {
+  let result = {};
+  for (let character of sentence) {
+    if (character !== " ") {
+      if (result[character]) {
+        result[character] += 1;
+      } else {
+        result[character] = 1;
+      }
+    }
+  }
+  return result;
+}
+
+let resultCount = countLetters("lighthouse in the house");
+console.log(resultCount);
+
+assertEqual(resultCount["i"], 2);
+assertEqual(resultCount["o"], 2);
