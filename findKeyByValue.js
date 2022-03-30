fd400a67c1ace2a7e9a84b510fd7c4eb9963b340
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-03-29 23:57:14 -0600

    lotide find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const findKeyByValue  = function(obj, val) {
+  for (let key in obj) {
+    console.log(key);
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
