fcbfd4bf512c4f05bc4c8283e71cd5e433915620
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-04-15 21:45:30 -0600

    Separated test cases in other file

--- letterPositions.js
+++ letterPositions.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual  = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-
 const letterPositions = function(sentence) {
   const results = {};
   // logic to update results here
@@ -36,10 +14,5 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
-
-const resultLetterPositions = letterPositions("lighthouse in the house");
-console.log(resultLetterPositions);
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
 
 module.exports = letterPositions;
