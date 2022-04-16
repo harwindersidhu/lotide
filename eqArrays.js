ef05e35ec96bebc1aa8797cb789b66a75b263960
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-04-16 01:08:35 -0600

    Updated eqArrays to make it work for nested Arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,17 @@
+//This functions check if two arrays are equal or not. It works for nested arrays as well.
+
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   } else {
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        if (!(eqArrays(arr1[i], arr2[i]))) {
+          return false;
+        }
+      } else if (Array.isArray(arr1[i]) || Array.isArray(arr2[i])) {
+        return false;
+      } else if (arr1[i] !== arr2[i]) {
         return false;
       }
     }
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -17,4 +17,20 @@ describe("#eqArrays", () => {
   it("returns true for arrays [] and []", () => {
     assert.equal(eqArrays([], []), true);
   });
+
+  it("returns true for arrays [[2, 3], [4]] and [[2, 3], [4]]", () => {
+    assert.equal(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+
+  it("returns false for arrays [[2, 3], [4]] and [[2, 3], [4, 5]]", () => {
+    assert.equal(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  });
+
+  it("returns false for arrays [[2, 3], [4]] and [[2, 3], 4]", () => {
+    assert.equal(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  });
+
+  it("returns true for arrays [[2, 3], [4], [[3, 4], [6,7]]] and [[2, 3], [4], [[3, 4], [6, 7]]]", () => {
+    assert.equal(eqArrays([[2, 3], [4], [[3, 4], [6,7]]], [[2, 3], [4], [[3, 4], [6, 7]]]), true);
+  });
 });
