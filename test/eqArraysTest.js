4d0f4e56f2c9a435b15500de1bfb666db62fa5a6
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-04-15 22:12:10 -0600

    Refactored files

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -3,18 +3,18 @@ const eqArrays = require('../eqArrays');
 
 describe("#eqArrays", () => {
   it("returns true for arrays [1, 2, 3] and [1, 2, 3]", () => {
-    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+    assert.equal(eqArrays([1, 2, 3], [1, 2, 3]), true);
   });
 
   it("returns false for arrays [1, 2, 3] and [1, 2]", () => {
-    assert.deepEqual(eqArrays([1, 2, 3], [1, 2]), false);
+    assert.equal(eqArrays([1, 2, 3], [1, 2]), false);
   });
 
   it("returns false for arrays [1, 2, 3] and [1, 2, 4]", () => {
-    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
+    assert.equal(eqArrays([1, 2, 3], [1, 2, 4]), false);
   });
 
   it("returns true for arrays [] and []", () => {
-    assert.deepEqual(eqArrays([], []), true);
+    assert.equal(eqArrays([], []), true);
   });
 });
