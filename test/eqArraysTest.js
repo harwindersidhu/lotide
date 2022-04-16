266e227613bab6ea900a9a5bde3ef3420cf6cf62
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-04-15 22:05:22 -0600

    Implemented Mocha and Chai for testing

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,20 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true);
-assertEqual(eqArrays([], []), true);
+describe("#eqArrays", () => {
+  it("returns true for arrays [1, 2, 3] and [1, 2, 3]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("returns false for arrays [1, 2, 3] and [1, 2]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2]), false);
+  });
+
+  it("returns false for arrays [1, 2, 3] and [1, 2, 4]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
+  });
+
+  it("returns true for arrays [] and []", () => {
+    assert.deepEqual(eqArrays([], []), true);
+  });
+});
