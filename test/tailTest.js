363a4fe3d8bec438e88939cdebec9c63feaff6dd
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-04-15 23:02:29 -0600

    did linting

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const tail = require('../tail');
 
 describe("#Tail", () => {
-  it ("returns length of words array which means tail function doesnot affect original array", () => {
+  it("returns length of words array which means tail function doesnot affect original array", () => {
     const words = ["Yo Yo", "Lighthouse", "Labs"];
     tail(words);
     assert.strictEqual(words.length, 3);
@@ -11,9 +11,9 @@ describe("#Tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
     const words = ["Yo Yo", "Lighthouse", "Labs"];
     assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
-  })
+  });
 
   it("returns [2, 3] for [1, 2, 3]", () => {
     assert.deepEqual(tail([1, 2, 3]), [2, 3]);
-  })
+  });
 });
